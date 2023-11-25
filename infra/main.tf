62d67cb9551883c0894675946750f42d475f1f7a
David <davidhaldorsen@gmail.com>
2023-11-25 18:19:37 +0100

    Not sure if policy document is the issue

--- infra/main.tf
+++ infra/main.tf
@@ -31,13 +31,13 @@ resource "aws_iam_role" "role_for_apprunner_service" {
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
+    actions = ["sts:AssumeRole"]
 
     principals {
-      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
     }
 
-    actions = ["sts:AssumeRole"]
   }
 }
 
