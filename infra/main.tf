ac291f31a9339d2c652b44cb0f8df4131e3b97c6
David <davidhaldorsen@gmail.com>
2023-11-25 17:56:22 +0100

    Forgot I pre-created the role...

--- infra/main.tf
+++ infra/main.tf
@@ -34,7 +34,7 @@ data "aws_iam_policy_document" "assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["tasks.apprunner.amazonaws.com"]
+      identifiers = ["lambda.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
