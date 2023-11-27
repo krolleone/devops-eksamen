de6417bfc2417bc63d151e26a5327aef0a25adb2
David <davidhaldorsen@gmail.com>
2023-11-27 21:41:12 +0100

    First try, implementing scans for all body parts

--- infra/dashboard.tf
+++ infra/dashboard.tf
@@ -29,6 +29,33 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total PPE Scans"
       }
+    },
+    {
+      "type": "metric",
+      "x": 12,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.dashboard_name}",
+            "total_full_scans.value"
+          ],
+          [
+            "${var.dashboard_name}",
+            "non_violations_all_regions.value"
+          ],
+          [
+            "${var.dashboard_name}",
+            "violations_all_regions.value"
+          ]
+        ],
+        "period": 60,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Total PPE Scans for all regions"
+      }
     }
   ]
 }
--- src/main/java/com/example/s3rekognition/controller/RekognitionController.java
+++ src/main/java/com/example/s3rekognition/controller/RekognitionController.java
@@ -17,10 +17,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 
 
@@ -34,6 +31,7 @@ public class RekognitionController implements ApplicationListener<ApplicationRea
 
 
     private Map<String, PPEClassificationResponse> ppeFaceScans = new HashMap();
+    private Map<String, PPEClassificationResponse> ppeAllRegionsScans = new HashMap();
 
     private MeterRegistry meterRegistry;
 
@@ -96,6 +94,62 @@ public class RekognitionController implements ApplicationListener<ApplicationRea
         return ResponseEntity.ok(ppeResponse);
     }
 
+    @GetMapping(value = "/scan-all-ppe", consumes = "*/*", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<PPEResponse> scanAllPPE(@RequestParam String bucketName) {
+        // List all objects in the S3 bucket
+        ListObjectsV2Result imageList = s3Client.listObjectsV2(bucketName);
+
+        // This will hold all of our classifications
+        List<PPEClassificationResponse> classificationResponses = new ArrayList<>();
+
+        // This is all the images in the bucket
+        List<S3ObjectSummary> images = imageList.getObjectSummaries();
+
+        // Iterate over each object and scan for PPE
+        for (S3ObjectSummary image : images) {
+            logger.info("scanning " + image.getKey());
+
+            // This is where the magic happens, use AWS rekognition to detect PPE
+            DetectProtectiveEquipmentRequest request = new DetectProtectiveEquipmentRequest()
+                    .withImage(new Image()
+                            .withS3Object(new S3Object()
+                                    .withBucket(bucketName)
+                                    .withName(image.getKey())))
+                    .withSummarizationAttributes(new ProtectiveEquipmentSummarizationAttributes()
+                            .withMinConfidence(80f)
+                            .withRequiredEquipmentTypes("FACE_COVER", "HAND_COVER", "HEAD_COVER"));
+
+            DetectProtectiveEquipmentResult result = rekognitionClient.detectProtectiveEquipment(request);
+
+            // If any person on an image lacks PPE on the face, hands or head, it's a violation of regulations
+            boolean violation = isTotalViolation(result);
+
+
+            logger.info("scanning " + image.getKey() + ", violation result " + violation);
+            // Categorize the current image as a violation or not.
+            int personCount = result.getPersons().size();
+            PPEClassificationResponse classification = new PPEClassificationResponse(image.getKey(), personCount, violation);
+            classificationResponses.add(classification);
+
+            ppeAllRegionsScans.put(image.getKey(), classification);
+        }
+        PPEResponse ppeResponse = new PPEResponse(bucketName, classificationResponses);
+        return ResponseEntity.ok(ppeResponse);
+    }
+
+    private static boolean isTotalViolation(DetectProtectiveEquipmentResult result) {
+        return result.getPersons().stream()
+                .flatMap(p -> p.getBodyParts().stream())
+                .anyMatch(bodyPart -> {
+                    String bodyPartName = bodyPart.getName();
+                    List<String> requiredParts = Arrays.asList("FACE", "HAND_COVER", "HEAD_COVER");
+                    return requiredParts.contains(bodyPartName) &&
+                            bodyPart.getEquipmentDetections().isEmpty();
+                });
+    }
+
+
     /**
      * Detects if the image has a protective gear violation for the FACE bodypart-
      * It does so by iterating over all persons in a picture, and then again over
@@ -118,11 +172,27 @@ public class RekognitionController implements ApplicationListener<ApplicationRea
         // Total Face-scans
         Gauge.builder("total_scans", ppeFaceScans,
                 b -> b.values().size()).register(meterRegistry);
+        // Total non-violations in Face-region
         Gauge.builder("non_violations", ppeFaceScans,
                 b -> b.values().stream()
                         .filter(v -> !v.isViolation())
                         .count()).register(meterRegistry);
+        // Total violations in Face-region
         Gauge.builder("violations", ppeFaceScans,
+                b -> b.values().stream()
+                        .filter(v -> v.isViolation())
+                        .count()).register(meterRegistry);
+
+        // Total scans all regions
+        Gauge.builder("total_full_scans", ppeAllRegionsScans,
+                b -> b.values().size()).register(meterRegistry);
+        // Total non-violations in all regions
+        Gauge.builder("non_violations_all_regions", ppeAllRegionsScans,
+                b -> b.values().stream()
+                        .filter(v -> !v.isViolation())
+                        .count()).register(meterRegistry);
+        // Total violations in all regions
+        Gauge.builder("violations_all_regions", ppeAllRegionsScans,
                 b -> b.values().stream()
                         .filter(v -> v.isViolation())
                         .count()).register(meterRegistry);
